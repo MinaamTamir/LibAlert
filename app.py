8991b4e58195673ca2b29a9fff47b1b8ffd931fd
Tamir <minaamtamir812@gmail.com>
2021-01-30 18:33:33 +0530

    with req

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from pathlib import Path
 from flask_pymongo import PyMongo
 import os
+from json import dumps
 import dns
 env_path=Path('.')/'.env'
 load_dotenv(dotenv_path=env_path)
@@ -22,10 +23,10 @@ def result_func():
     #get input from form   
     name=request.form.get("libname")
     result=table.find({"Name":name})
-    print(result)
     result_list = []
     for item in result:
         result_list.append(item)
+    print(result_list)
     return render_template("result.html",name=name,result_list=result_list)
 @app.route('/addonlyforadmin',methods=["POST","GET"])
 def add():
@@ -43,4 +44,4 @@ def add():
     return render_template("add.html")
 
 if __name__=="__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0",port="5002",debug=True)
